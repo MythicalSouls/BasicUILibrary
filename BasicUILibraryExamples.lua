cb9e9e01e17a07e0d1db9ffb6ddfaef60ecdcde8
MythicalSouls <dyzzang2012@gmail.com>
2025-08-12 23:25:23 +0900

    Create BasicUILibraryExamples.lua

---
+++ BasicUILibraryExamples.lua
@@ -0,0 +1,32 @@
+--The main library code goes here
+local function aaa()
+	singlebutton("Developer Console", DoThing, function()
+		game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
+	end, nil)
+	singlebutton("RemoteEvent Inspection (Dev Console)", DoThing, function()
+		warn("activating RemoteEvent Inspection (Dev Console)")
+		local a
+		a = hookmetamethod(game, "__namecall", function(self, ...)
+			local b = getnamecallmethod()
+			local arglist = {...}
+			if b == "FireServer" then
+				print("calling script: "..tostring(getcallingscript(b):GetFullName()))
+				print("args: "..tostring(unpack(arglist)))
+				print("remote name: "..tostring(self:GetFullName()))
+				for i,v in pairs(arglist) do
+					print("type of argument #"..tostring(i)..": "..tostring(type(v)))
+				end
+			end
+			return a(self, ...)
+		end)
+		warn("activated RemoteEvent Inspection (Dev Console)")
+	end, nil)
+end
+local function executortabfunctions()
+	local aa = singlebuttonwithtextbox("Execute", DoThing, function() end, nil)
+	aa.Button.MouseButton1Click:Connect(function()
+		loadstring(aa.Box.Text)();
+	end)
+end
+createtab("Debuggers", aaa, nil)
+createtab("Executor", executortabfunctions, nil)
